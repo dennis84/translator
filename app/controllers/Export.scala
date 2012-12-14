e99dd8453f72726c7104733e35cb7a587fb3b7c6
Dennis Dietrich <d.dietrich84@googlemail.com>
2012-12-14 14:34:47 +0100

    update

--- app/controllers/Export.scala
+++ app/controllers/Export.scala
@@ -6,9 +6,8 @@ import translator.forms._
 
 object ExportController extends BaseController {
 
-  def translations(project: String) = SecuredWithProject(project) { (_user, _project, _projects, _req) =>
-    implicit val (user, project, req) = (_user, _project, _req)
-
+  def translations(project: String) = SecuredWithProject(project) { (user, project, projects, _req) =>
+    implicit val req = _req
     JsonOk(TranslationDAO.findAllByProject(project).filter(_.code == getOr("language", "en")).map { trans =>
       trans.name -> trans.text
     }.toMap)
