27c45044ad98fa1c544c2713e727cd60d17aa64b
Dennis Dietrich <d.dietrich84@googlemail.com>
2012-12-17 22:42:25 +0100

    removed some imports

--- app/controllers/Search.scala
+++ app/controllers/Search.scala
@@ -1,8 +1,5 @@
 package translator.controllers
 
-import org.elasticsearch.index.query._, FilterBuilders._, QueryBuilders._
-import org.elasticsearch.search._, facet._, terms._, sort._, SortBuilders._, builder._
-import com.mongodb.casbah.Imports._
 import translator._
 import translator.models._
 
